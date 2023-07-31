237421e02307111527c0e0de8afb22eba89d9dd1
gracekim527 <lemonherb0323@naver.com>
2023-07-31 16:41:34 +0900

    :white_check_mark: [Add] Day02 - Mission_DatePicker

--- Day02/DatePicker/DatePicker/ViewController.swift
+++ Day02/DatePicker/DatePicker/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     let timeSelector: Selector = #selector(ViewController.updateTime)
     let interval = 1.0
     var count = 0
+    var alarmTime: String?
     
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblPickerTime: UILabel!
@@ -32,6 +33,15 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date) // 앞서 설정한 날짜 형식에 따라 date 변수의 값을 문자열로 변환
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        
+        if (alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
     }
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
@@ -40,6 +50,9 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
         lblPickerTime.text = "선택시간 " + formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
     }
     
 }
